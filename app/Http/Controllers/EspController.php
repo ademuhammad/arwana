ab336d879791a22fff3d96dc2c72b7d5c893b0f3
Ade Muhamad Nurhadi <ademnurhadi@gmail.com>
2023-07-02 17:03:51 +0700

    sa

--- app/Http/Controllers/EspController.php
+++ app/Http/Controllers/EspController.php
@@ -2,20 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Http\Request;
-use Exception;
 use App\Models\Esp;
 use App\Events\NewEspEvent;
 use Illuminate\Support\Facades\Event;
-use \Carbon\Carbon;
+
 
 class EspController extends Controller
 {
     public function store(Request $request)
     {
         try {
-            $currentDateTime = \Carbon\Carbon::now('Asia/Jakarta')->format('Y-m-d H:i:s');
+
 
             $esp = Esp::create([
                 'final_ph' => $request->final_ph,
@@ -32,7 +30,7 @@ class EspController extends Controller
                 'code' => 200,
                 'esp' => $esp,
             ]);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             return response()->json([
                 'code' => 400,
                 'message' => $e->getMessage(),
