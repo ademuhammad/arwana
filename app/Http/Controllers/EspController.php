f2d864c82a3fe3379a33193103b64f99baeed06a
Ade Muhamad Nurhadi <ademnurhadi@gmail.com>
2023-07-05 23:59:30 +0700

    asas

--- app/Http/Controllers/EspController.php
+++ app/Http/Controllers/EspController.php
@@ -23,14 +23,16 @@ class EspController extends Controller
                 'kualitasair' => $request->kualitasair,
             ]);
 
+            $pompa = Pompa::first();
 
+            $data = $pompa->pompafilter . ',' . $pompa->pompaisi . ',' . $pompa->pompabuang;
 
 
 
             Event::dispatch(new NewEspEvent($esp));
 
             return response()->json(
-                $esp
+                $data
             );
         } catch (\Exception $e) {
             return response()->json([
@@ -42,33 +44,7 @@ class EspController extends Controller
 
     public function pompacontrol(Request $request)
     {
-        try {
-            $pompa = Pompa::create([
-                'pompafilter' => $request->pompafilter,
-                'pompabuang' => $request->pompabuang,
-                'pompaisi' => $request->pompaisi,
-
-            ]);
-            $pompa = Pompa::first();
-
-            $data = $pompa->pompafilter . ',' . $pompa->pompaisi . ',' . $pompa->pompabuang;
-            // $pompa = Pompa::first();
-
-            // $data = $pompa->pompafilter . ',' . $pompa->pompaisi . ',' . $pompa->pompabuang;
-
-
-
-
-            return response()->json(
-                $data
-            );
-        } catch (\Exception $e) {
-            return response()->json([
-                'code' => 400,
-                'message' => $e->getMessage(),
-            ]);
-        }
-
+        $pompa = Pompa::first();
         // Memperbarui nilai-nilai Pompa berdasarkan data permintaan (request)
         $pompa->pompafilter = $request->has('pompa_1') && $request->pompa_1 == 'on' ? 'HIGH' : 'LOW';
         $pompa->pompabuang = $request->has('pompa_2') && $request->pompa_2 == 'on' ? 'HIGH' : 'LOW';
